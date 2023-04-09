c101d9b0a23a15318ca7f2dca96b37f9a75a147f
taha <rtdonuk@gmail.com>
2023-04-09 22:37:25 +0300

    initial edits

--- src/main/java/github/tdonuk/stringhelper/Base64DecodeAction.java
+++ src/main/java/github/tdonuk/stringhelper/Base64DecodeAction.java
@@ -1,2 +1,51 @@
-package github.tdonuk.stringhelper;public class Base64DecodeAction {
+package github.tdonuk.stringhelper;
+
+import com.intellij.openapi.actionSystem.DataContext;
+import com.intellij.openapi.command.WriteCommandAction;
+import com.intellij.openapi.editor.Caret;
+import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.editor.SelectionModel;
+import com.intellij.openapi.editor.actionSystem.EditorAction;
+import com.intellij.openapi.editor.actionSystem.EditorActionHandler;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.project.ProjectManager;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
+public class Base64DecodeAction extends EditorAction {
+	
+	public Base64DecodeAction() {
+		super(new EditorActionHandler() {
+			@Override
+			protected void doExecute(@NotNull Editor editor, @Nullable Caret caret, DataContext dataContext) {
+				final Project[] openProjects = ProjectManager.getInstance().getOpenProjects();
+				if (openProjects.length <= 0) {
+					return;
+				}
+				
+				final String selectedText = editor.getSelectionModel().getSelectedText();
+				
+				final Document document = editor.getDocument();
+				
+				if (selectedText != null && !selectedText.isEmpty()) {
+					WriteCommandAction.runWriteCommandAction(openProjects[0], () -> {
+						if (!document.isWritable()) {
+							return;
+						}
+						
+						final SelectionModel selectionModel = editor.getSelectionModel();
+						document.replaceString(selectionModel.getSelectionStart(), selectionModel.getSelectionEnd(), new String(Base64.getDecoder().decode(selectedText)));
+					});
+				}
+			}
+		});
+	}
+	
+	protected Base64DecodeAction(EditorActionHandler defaultHandler) {
+		super(defaultHandler);
+	}
 }
--- src/main/java/github/tdonuk/stringhelper/Base64EncodeAction.java
+++ src/main/java/github/tdonuk/stringhelper/Base64EncodeAction.java
@@ -1,2 +1,51 @@
-package github.tdonuk.stringhelper;public class Base64EncodeAction {
+package github.tdonuk.stringhelper;
+
+import com.intellij.openapi.actionSystem.DataContext;
+import com.intellij.openapi.command.WriteCommandAction;
+import com.intellij.openapi.editor.Caret;
+import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.editor.SelectionModel;
+import com.intellij.openapi.editor.actionSystem.EditorAction;
+import com.intellij.openapi.editor.actionSystem.EditorActionHandler;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.project.ProjectManager;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
+public class Base64EncodeAction extends EditorAction {
+	
+	public Base64EncodeAction() {
+		super(new EditorActionHandler() {
+			@Override
+			protected void doExecute(@NotNull Editor editor, @Nullable Caret caret, DataContext dataContext) {
+				final Project[] openProjects = ProjectManager.getInstance().getOpenProjects();
+				if (openProjects.length <= 0) {
+					return;
+				}
+				
+				final String selectedText = editor.getSelectionModel().getSelectedText();
+				
+				final Document document = editor.getDocument();
+				
+				if (selectedText != null && !selectedText.isEmpty()) {
+					WriteCommandAction.runWriteCommandAction(openProjects[0], () -> {
+						if (!document.isWritable()) {
+							return;
+						}
+						
+						final SelectionModel selectionModel = editor.getSelectionModel();
+						document.replaceString(selectionModel.getSelectionStart(), selectionModel.getSelectionEnd(), Base64.getEncoder().encodeToString(selectedText.getBytes(StandardCharsets.UTF_8)));
+					});
+				}
+			}
+		});
+	}
+	
+	protected Base64EncodeAction(EditorActionHandler defaultHandler) {
+		super(defaultHandler);
+	}
 }
